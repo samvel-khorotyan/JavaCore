cc530cd812a6d365de8742ab349379238e3fde1a
Samvel Khorotyan <samvelkhorotyan@gmail.com>
2021-10-04 00:26:05 +0400

    lesson2

---
+++ src/lesson2/Example.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        int nam = 1;
+        if (nam < 100) {
+            System.out.println("нам менше 100");
+        }
+    }
+}
+
---
+++ src/lesson3/Intexample.java
@@ -0,0 +1,8 @@
+package lesson3;
+
+public class Intexample {
+
+    public static void main(String [] args){
+
+    }
+}
