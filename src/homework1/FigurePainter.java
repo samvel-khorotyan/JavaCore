f7fb779eb65f33595f10c5dc09b54782089cd684
Samvel Khorotyan <samvelkhorotyan@gmail.com>
2021-10-07 21:39:17 +0400

    FigurePainter

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -17,7 +17,7 @@ public class FigurePainter {
         for (int i = 0; i < 4; i++) {
 
             for (int j = 3; j >= i; j--) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k = 0; k <= i; k++) {
                 System.out.print(" *");
