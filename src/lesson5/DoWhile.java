73ad6e658928fdab81a9ad0bfaedbbfdb2ad3f26
Samvel Khorotyan <samvelkhorotyan@gmail.com>
2021-10-15 23:46:22 +0400

    arrayutil

---
+++ src/lesson5/DoWhile.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        do {
+            System.out.println("такт " + n);
+        } while (--n > 0);
+    }
+}
---
+++ src/lesson5/ForTick.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class ForTick {
+
+    public static void main(String[] args) {
+
+        int n;
+        for (n = 10; n > 0; n--)
+            System.out.println("такт " + n);
+
+    }
+}
