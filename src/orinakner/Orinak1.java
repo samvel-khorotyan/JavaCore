ec62a535469175b9ac14d309d4eae3085ea5e206
Samvel Khorotyan <samvelkhorotyan@gmail.com>
2021-10-20 00:02:54 +0400

    charUtil

--- src/orinakner/Orinak1.java
+++ src/orinakner/Orinak1.java
@@ -5,15 +5,5 @@ public class Orinak1 {
     public static void main(String[] args) {
 
 
-        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
-        char[] result = new char[9];
-        int i, j;
-        for (j = 0, i = 0; i < text.length; j++, i++) {
-            if (text[i] == ' ') {
-                continue;
-            }
-            result[j] = text[i];
-            System.out.print(result[j] + " ");
-        }
     }
 }
